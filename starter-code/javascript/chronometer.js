c231e2c8f8071da00c1aea137366f7082f0037e3
“Alex <prop.aburdett@gmail.com>
2019-01-14 15:30:42 +0100

    completed

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,4 +1,45 @@
 // Constructor
+class Chronometer{
+  constructor(){
+  this.currentTime = 0
+  this.intervalId = 0  
+}
+startClick(){
+  var intervalId = setInterval(()=>{
+   this.currentTime ++ 
+    },1000)
+  }
+
+setMinutes(){
+  var timeToMinutes = this.currentTime/60
+  timeToMinutes = parseInt(timeToMinutes)
+  if(timeToMinutes !== 0){
+  return timeToMinutes}
+  else{return 0}
+}
+
+setSeconds(){
+return Math.floor(this.currentTime % 60)
+}
+
+//twoDigitsNumber 
+twoDigitsNumber(value){
+  if (value < 10){
+    return ("0"+value)
+  }
+  return value.toString()
+}
+
+setTime(){
+  var minutes = this.setMinutes()
+  var seconds = this.setSeconds() 
+  minutes.twoDigitsNumber()
+  seconds.twoDigitsNumber()
+
+}
+
+}
+
 // function Chronometer() {
 
 // }
